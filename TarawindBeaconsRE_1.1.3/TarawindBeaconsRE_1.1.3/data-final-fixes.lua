f1d0f68b1e21afdee31c1e950a9d7c59f0a8be0b
nattanang <120257866+nattanang@users.noreply.github.com>
2025-04-15 09:34:59 +0700

    Update data-final-fixes.lua

--- TarawindBeaconsRE_1.1.3/TarawindBeaconsRE_1.1.3/data-final-fixes.lua
+++ TarawindBeaconsRE_1.1.3/TarawindBeaconsRE_1.1.3/data-final-fixes.lua
@@ -1,11 +1,9 @@
--- Productivity fix
+-- Productivity & Quality fix
 for k, v in pairs(data.raw.module) do
   if v.name:find("productivity%-module") then
     v.limitation = nil -- empty limitation table
     v.limitation_message_key = nil
   end
-end
--- Quality fix
 for k, v in pairs(data.raw.module) do
   if v.name:find("quality%-module") then
     v.limitation = nil -- empty limitation table
